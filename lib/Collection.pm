9152bb8be81bdf29240148ac88ad82248b72c0e3
Zahatski Aliaksandr <zagap@users.sourceforge.net>
2008-09-30 08:07:56 +0000

    Import of ZAG/Collection-0.39 from CPAN.
    gitpan-cpan-distribution: Collection
    gitpan-cpan-version:      0.39
    gitpan-cpan-path:         ZAG/Collection-0.39.tar.gz
    gitpan-cpan-author:       ZAG
    gitpan-cpan-maturity:     released

--- lib/Collection.pm
+++ lib/Collection.pm
@@ -1,6 +1,6 @@
 package Collection;
 
-#$Id: Collection.pm 341 2008-09-29 14:18:37Z zag $
+#$Id: Collection.pm 342 2008-09-30 08:02:12Z zag $
 
 =head1 NAME
 
@@ -85,7 +85,7 @@ use Collection::Utl::ActiveRecord;
 use Collection::Utl::Base;
 use Collection::Utl::LazyObject;
 @Collection::ISA     = qw(Collection::Utl::Base);
-$Collection::VERSION = '0.38';
+$Collection::VERSION = '0.39';
 attributes qw( _obj_cache );
 
 sub _init {
